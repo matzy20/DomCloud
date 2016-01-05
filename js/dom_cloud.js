5d6b5db970361abc3b8912d4963b515cbe36737f
Gail Matsushima <gail.matsushima@gmail.com>
2016-01-04 20:59:14 -1000

    elements are adding but problem with unexpected identifier in tryig to create funcntion to add attributes

--- js/dom_cloud.js
+++ js/dom_cloud.js
@@ -6,7 +6,9 @@
 //need a starting point, below = html
 var treeRoot = document.documentElement;
 var domCloud = {
+
   elementTracker: {},
+  attributeTracker: {},
   //method on object domCloud
   findingElements: function (element) {
 
@@ -14,30 +16,38 @@ var domCloud = {
 
     if (domCloud.elementTracker.hasOwnProperty(elementType)){
       domCloud.elementTracker[elementType]++;
-      console.log('checking if same elementType');
     } else{
       //setting it to 1 vs 'adding' to it
       domCloud.elementTracker[elementType] = 1;
     }
-
-
-    console.log(element);
-    console.log("length", element.children.length);
+    //since we're pulling via tagName no need a base? Similar to attribute?
+    /*console.log("length", element.children.length);
+    used the above to check what's printing out*/
     for (var i = 0; i < element.children.length; i++){
       domCloud.findingElements(element.children[i]);
     }
   }
 
-  // fndingAttributes: function (attribute){
+  findingAttributes: function (attribute) {
 
-  //   var attributeType = element.attribute;
-  // }
+    var checkAttribute = attribute.getAttribute();
+    console.log(checkAttribute);
 
-
+    if (domCloud.attributeTracker.hasOwnProperty(checkAttribute)){
+      domCloud.attributeTracker[checkAttribute]++;
+    } else {
+      domCloud.attributeTracker[checkAttribute] = 1;
+    }
+    for (var i = 0; i < checkAttribute.children.length; i++){
+      domCloud.findingAttributes(attribute.children[i]);
+    }
+  }
 };
 //need window onload since <script> was located at top of html
 //dom_cloud.js wasn't loading fully, would hit script then break out due to location of script on html NOT b/c NO CHILD
 window.onload = function (){
   domCloud.findingElements(treeRoot);
+  domCloud.findingAttributes(treeRoot);
   console.log(domCloud.elementTracker);
+  console.log(domCloud.attributeTracker);
 };
